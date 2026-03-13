0b8f0ab4c10606d07031cd7da54b9f05848c44b8
displayline <matthewfuggenbaker@gmail.com>
2026-03-13 08:53:39 +0200

    Add files via upload

---
+++ _next/static/chunks/19-84092f69f7e8ab33.js
@@ -0,0 +1,64 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[19],{5117:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("ArrowUp",[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]])},1798:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Facebook",[["path",{d:"M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z",key:"1jg4f8"}]])},6539:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Instagram",[["rect",{width:"20",height:"20",x:"2",y:"2",rx:"5",ry:"5",key:"2e1cvw"}],["path",{d:"M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z",key:"9exkf1"}],["line",{x1:"17.5",x2:"17.51",y1:"6.5",y2:"6.5",key:"r4j83e"}]])},7461:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Linkedin",[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]])},1295:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]])},6142:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]])},8004:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]])},2741:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Phone",[["path",{d:"M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z",key:"foiqr5"}]])},3039:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Twitter",[["path",{d:"M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z",key:"pff0z6"}]])},2549:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]])},9199:function(e,t,n){n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.302.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(2898).Z)("Youtube",[["path",{d:"M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17",key:"1q2vi4"}],["path",{d:"m10 15 5-3-5-3z",key:"1jp15x"}]])},2167:function(e,t,n){n.d(t,{M:function(){return m}});var r=n(2265),i=n(538);function u(){let e=(0,r.useRef)(!1);return(0,i.L)(()=>(e.current=!0,()=>{e.current=!1}),[]),e}var o=n(2363),c=n(8243),l=n(961);class a extends r.Component{getSnapshotBeforeUpdate(e){let t=this.props.childRef.current;if(t&&e.isPresent&&!this.props.isPresent){let e=this.props.sizeRef.current;e.height=t.offsetHeight||0,e.width=t.offsetWidth||0,e.top=t.offsetTop,e.left=t.offsetLeft}return null}componentDidUpdate(){}render(){return this.props.children}}function f({children:e,isPresent:t}){let n=(0,r.useId)(),i=(0,r.useRef)(null),u=(0,r.useRef)({width:0,height:0,top:0,left:0});return(0,r.useInsertionEffect)(()=>{let{width:e,height:r,top:o,left:c}=u.current;if(t||!i.current||!e||!r)return;i.current.dataset.motionPopId=n;let l=document.createElement("style");return document.head.appendChild(l),l.sheet&&l.sheet.insertRule(`
+          [data-motion-pop-id="${n}"] {
+            position: absolute !important;
+            width: ${e}px !important;
+            height: ${r}px !important;
+            top: ${o}px !important;
+            left: ${c}px !important;
+          }
+        `),()=>{document.head.removeChild(l)}},[t]),r.createElement(a,{isPresent:t,childRef:i,sizeRef:u},r.cloneElement(e,{ref:i}))}let s=({children:e,initial:t,isPresent:n,onExitComplete:i,custom:u,presenceAffectsLayout:o,mode:a})=>{let s=(0,l.h)(d),h=(0,r.useId)(),p=(0,r.useMemo)(()=>({id:h,initial:t,isPresent:n,custom:u,onExitComplete:e=>{for(let t of(s.set(e,!0),s.values()))if(!t)return;i&&i()},register:e=>(s.set(e,!1),()=>s.delete(e))}),o?void 0:[n]);return(0,r.useMemo)(()=>{s.forEach((e,t)=>s.set(t,!1))},[n]),r.useEffect(()=>{n||s.size||!i||i()},[n]),"popLayout"===a&&(e=r.createElement(f,{isPresent:n},e)),r.createElement(c.O.Provider,{value:p},e)};function d(){return new Map}var h=n(781),p=n(6567);let y=e=>e.key||"",m=({children:e,custom:t,initial:n=!0,onExitComplete:c,exitBeforeEnter:l,presenceAffectsLayout:a=!0,mode:f="sync"})=>{var d;(0,p.k)(!l,"Replace exitBeforeEnter with mode='wait'");let m=(0,r.useContext)(h.p).forceRender||function(){let e=u(),[t,n]=(0,r.useState)(0),i=(0,r.useCallback)(()=>{e.current&&n(t+1)},[t]);return[(0,r.useCallback)(()=>o.Wi.postRender(i),[i]),t]}()[0],k=u(),x=function(e){let t=[];return r.Children.forEach(e,e=>{(0,r.isValidElement)(e)&&t.push(e)}),t}(e),v=x,E=(0,r.useRef)(new Map).current,Z=(0,r.useRef)(v),w=(0,r.useRef)(new Map).current,M=(0,r.useRef)(!0);if((0,i.L)(()=>{M.current=!1,function(e,t){e.forEach(e=>{let n=y(e);t.set(n,e)})}(x,w),Z.current=v}),d=()=>{M.current=!0,w.clear(),E.clear()},(0,r.useEffect)(()=>()=>d(),[]),M.current)return r.createElement(r.Fragment,null,v.map(e=>r.createElement(s,{key:y(e),isPresent:!0,initial:!!n&&void 0,presenceAffectsLayout:a,mode:f},e)));v=[...v];let g=Z.current.map(y),R=x.map(y),z=g.length;for(let e=0;e<z;e++){let t=g[e];-1!==R.indexOf(t)||E.has(t)||E.set(t,void 0)}return"wait"===f&&E.size&&(v=[]),E.forEach((e,n)=>{if(-1!==R.indexOf(n))return;let i=w.get(n);if(!i)return;let u=g.indexOf(n),o=e;o||(o=r.createElement(s,{key:y(i),isPresent:!1,onExitComplete:()=>{E.delete(n);let e=Array.from(w.keys()).filter(e=>!R.includes(e));if(e.forEach(e=>w.delete(e)),Z.current=x.filter(t=>{let r=y(t);return r===n||e.includes(r)}),!E.size){if(!1===k.current)return;m(),c&&c()}},custom:t,presenceAffectsLayout:a,mode:f},i),E.set(n,o)),v.splice(u,0,o)}),v=v.map(e=>{let t=e.key;return E.has(t)?e:r.createElement(s,{key:y(e),isPresent:!0,presenceAffectsLayout:a,mode:f},e)}),r.createElement(r.Fragment,null,E.size?v:v.map(e=>(0,r.cloneElement)(e)))}}}]);
